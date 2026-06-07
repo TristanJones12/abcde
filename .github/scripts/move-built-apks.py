99e1ae5b9b4e66c58280516597e0efd834c00029
TristanJones12 <bobjamal2323@gmail.com>
2026-06-07 20:50:05 +0200

    Update move-built-apks.py

--- .github/scripts/move-built-apks.py
+++ .github/scripts/move-built-apks.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 import shutil
 
+# Relative to workspace root, not home directory
+APK_ARTIFACTS_DIR = Path("apk-artifacts")
 REPO_APK_DIR = Path("repo/apk")
 
 shutil.rmtree(REPO_APK_DIR, ignore_errors=True)
 REPO_APK_DIR.mkdir(parents=True, exist_ok=True)
 
-for apk in Path("apk-artifacts").glob("**/*.apk"):
+for apk in APK_ARTIFACTS_DIR.glob("**/*.apk"):
     apk_name = apk.name.replace("-release.apk", ".apk")
-    shutil.move(str(apk), REPO_APK_DIR.joinpath(apk_name))
-    
+    shutil.move(str(apk), REPO_APK_DIR / apk_name)
+
+print(f"Moved APKs: {list(REPO_APK_DIR.iterdir())}")
